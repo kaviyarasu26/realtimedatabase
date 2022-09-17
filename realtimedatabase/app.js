59209b8ad35f5b91696d760c853402669c91ac7e
kaviyarasu26 <101913405+kaviyarasu26@users.noreply.github.com>
2022-09-17 18:46:21 +0530

    realtimedatabase

---
+++ realtimedatabase/app.js
@@ -0,0 +1,58 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCjUmWiSA0w3KaMREKX6JC7LFMqPaJ2NR0",
+    authDomain: "fir-database-419d5.firebaseapp.com",
+    databaseURL: "https://fir-database-419d5-default-rtdb.firebaseio.com",
+    projectId: "fir-database-419d5",
+    storageBucket: "fir-database-419d5.appspot.com",
+    messagingSenderId: "1000973370272",
+    appId: "1:1000973370272:web:254ef208cbd69031619ac5",
+    measurementId: "G-N4HXP5J7TS"
+  };
+firebase.initializeApp(firebaseConfig);
+
+
+
+var display=document.querySelector('#display');
+
+
+
+function dataToFirebase(work,time ){
+    var id=new Date();
+    firebase.database().ref("doc/"+id.getTime()).set({
+        work: work,
+        time: time
+    })
+
+}
+function insert(work,time){
+    display.innerHTML+=`<div>${work}<br>${time}</div><hr>`
+}
+
+function retrive(){
+   //read
+    firebase.database().ref("doc/").once('value', (snap) => {
+       
+        console.log(snap.val());
+        display.innerHTML="";
+        for(var i in snap.val()){
+            insert(snap.val()[i]["work"],snap.val()[i]["time"]);
+
+        }
+    })
+
+}
+retrive();
+
+
+
+document.getElementById("add").addEventListener('click',()=>{
+    console.log("Kaviyarasu");
+    dataToFirebase(document.getElementById("work-el").value,document.getElementById("time-el").value);
+
+})
+
+
+
+
+
